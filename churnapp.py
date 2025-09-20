ea9aa5311368998cc772a1dfccdbfdcb152781c8
P SOWMITH <135353249+Sowmith123@users.noreply.github.com>
2025-09-20 10:31:27 +0530

    Update churnapp.py

--- churnapp.py
+++ churnapp.py
@@ -2,7 +2,8 @@
 
 import streamlit as st
 import pandas as pd
-import numpy as np"""
+import numpy as np
+"""
 Telecommunication Churn Dashboard (v1.0)
 ----------------------------------------
 Author: Your Name
